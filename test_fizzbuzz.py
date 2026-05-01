229187d9e8ba69813d1560647be3848d88833606
aksharpatel2006 <patel.a54@nulondon.ac.uk>
2026-05-01 21:11:01 +0100

    Initial unit Tests

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,2 +1,10 @@
+from program import fizzbuzz
+
 def test_smoke():
     assert 2+2 == 4
+
+def test_fizzbuzz_happy():
+    assert fizzbuzz(3) == "Buzz"
+
+def test_fizzbuzz_unhappy():
+    assert fizzbuzz("String") == None
