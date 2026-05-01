40d64bf6bc10953a5d298e0bf52b7f4de1a61a6e
aksharpatel2006 <patel.a54@nulondon.ac.uk>
2026-05-01 22:52:07 +0100

    Added comments (technical documentation)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,17 @@
-def fizzbuzz_function(num):
-    if num % 3 == 0 and num % 5 == 0:
+#FizzBuzz program
+def fizzbuzz_function(num): # FizzBuzz function
+    """
+    This function checks if the input value is a multiple of 3, 5 or both 3 & 5.
+    If the number is a multiple of 3, the function returns "Fizz"
+    If the number is a multiple of 5, the function returns "Buzz"
+    If the number is a multiple of both 3 & 5, the function returns "FizzBuzz"
+    If the number is NOT a multiple of any of these values, the function returns the input value
+    """
+    if num % 3 == 0 and num % 5 == 0: # Checks if num is a multiple of both 3 and 5
         return "FizzBuzz"
-    elif num % 3 == 0:
+    elif num % 3 == 0: # Checks if num is a multiple of 3
         return "Fizz"
-    elif num % 5 == 0:
+    elif num % 5 == 0: # Checks if num is a multiple of both 5
         return "Buzz"
-    else:
+    else: # If the number is NOT a multiple of any of these values, the function returns the input value
         return num
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,15 +1,17 @@
-from fizzbuzz import fizzbuzz_function
+from fizzbuzz import fizzbuzz_function # Importing the fizzbuzz function
 
-def test_smoke():
+def test_smoke(): # Smoke test
     assert 2+2 == 4
 
-def test_fizzbuzz_happy():
+def test_fizzbuzz_happy(): # Test function to check for "Fizz", "Buzz" or "FizzBuzz" outputs
+    # The assertions below check if outputs of the function match our expected results (via the comparison operator)
     assert fizzbuzz_function(9) == "Fizz"
     assert fizzbuzz_function(15) == "FizzBuzz"
     assert fizzbuzz_function(25) == "Buzz"
     assert fizzbuzz_function(27) == "Fizz"
 
-def test_fizzbuzz_boring():
+def test_fizzbuzz_boring(): # Test function to check if outputs are the same as inputs
+     # The assertions below check if outputs of the function match our expected results (via the comparison operator)
     assert fizzbuzz_function(7) == 7
     assert fizzbuzz_function(14) == 14
     assert fizzbuzz_function(19) == 19
