9d7bb9659a782b2e8fe0bbe4e77f7eb651815b8c
aksharpatel2006 <patel.a54@nulondon.ac.uk>
2026-05-01 21:33:49 +0100

    Function Updates

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,7 @@
 def fizzbuzz_function(num):
-    return "Fizz"
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return "Buzz"
