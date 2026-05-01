620ef247c316758635f0eebbfa872a406b344bc8
aksharpatel2006 <patel.a54@nulondon.ac.uk>
2026-05-01 21:43:12 +0100

    Unhappy Test

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -7,3 +7,7 @@ def test_fizzbuzz_happy():
     assert fizzbuzz_function(9) == "Fizz"
     assert fizzbuzz_function(15) == "FizzBuzz"
     assert fizzbuzz_function(25) == "Buzz"
+
+def test_fizzbuzz_unhappy():
+    assert fizzbuzz_function(7) == 7
+    assert fizzbuzz_function("seven") == None
